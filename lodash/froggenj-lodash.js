3a574559745db25ff714f3aca4773276efd3257c
froggenJ <947493843@qq.com>
2019-07-16 22:01:53 +0800

    s

--- lodash/froggenj-lodash.js
+++ lodash/froggenj-lodash.js
@@ -1,5 +1,13 @@
 var froggenj = {
   compact : function (arr) {
     return arr.filter(it=>it)
-  }
+  },
+
+  every : function function (ary,predicate) {
+    ary.reduce((result,item)=>{return result && predicate(item)},true)
+  },
+
+  some : function (argument) {
+    ary.reduce((result,item)=>{return result && predicate(item)},false)
+  },
 }
