615fa1b18f4c09c67b8955fecda59d97de906452
froggenJ <947493843@qq.com>
2019-07-16 22:26:34 +0800

    s

--- lodash/froggenj-lodash.js
+++ lodash/froggenj-lodash.js
@@ -1,13 +1,63 @@
 var froggenj = {
-  compact : function (arr) {
+  compact:function (arr) {
     return arr.filter(it=>it)
   },
 
-  every : function function (ary,predicate) {
+  faltten:function (ary) {
+    var result = []
+    for(var val of ary){
+      if(Array.isArray(val)){
+        result.push(...val)
+      }else{
+        result.push(val)
+      }
+    }
+    return result
+  },
+
+  falttenDeep:function (ary) {
+    var result = []
+    for(var val of ary){
+      if(Array.isArray(val)){
+        var flattenVal = falttenDeep(val)
+        result.push(...flattenVal)
+      }else{
+        result.push(val)
+      }
+    }
+    return result
+  },
+
+  falttenDepth:function (ary,depth = 1) {
+    var result = []
+    for(var val of ary){
+      if(Array.isArray(val)){
+        var falttenVal = falttenDepth(val,depth-1)
+        result.push(...falttenVal)
+      }else{
+        result.push(val)
+      }
+    }
+    return result
+  },
+
+
+
+
+
+  every:function (ary,predicate) {
     ary.reduce((result,item)=>{return result && predicate(item)},true)
   },
 
-  some : function (argument) {
-    ary.reduce((result,item)=>{return result && predicate(item)},false)
+  some:function (ary,predicate) {
+    // ary.reduce((result,item)=>{return result || predicate(item)},false)
+    return !every(ary,negate(predicate))
   },
+
+  negate:function (f) {
+    return function (...args) {
+      return !f(...args)
+    }
+  },
+
 }
